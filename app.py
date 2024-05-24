956f2eede0dacde74a01981f17643fcd28da0315
Nathan Souza <naathanmarinho11@gmail.com>
2024-05-24 13:19:13 +0000

    adiciona processo de tratamento  das mensagens

--- app.py
+++ app.py
@@ -59,3 +59,65 @@ def get_answer(llm, messages) -> tuple[str, float]:
         return answer.content, cb.total_cost
     if isinstance(llm, LlamaCpp):
         return llm(llama_v2_prompt(convert_langchainschema_to_dict(messages))), 0.0
+
+
+# Identifica o tipo de uma mensagem
+def find_role (message: Union[SystemMessage, HumanMessage, AIMessage]) -> str:
+    """
+    Identify role name from langchain.schema object.
+    """
+    if isinstance (message, SystemMessage):
+        return "system"
+    if isinstance(message, HumanMessage):
+        return "user"
+    if isinstance(message, AIMessage):
+        return "assistant"
+    raise TypeError("Unknown message type.")
+
+
+# Converte uma lista de mensagens para lista de dicionários
+def convert_langchainschema_to_dict(
+        messages: List[Union[SystemMessage, HumanMessage, AIMessage]]) \ -> List[dict]:
+    """
+    Convert the chain of chat messages in list of langchain.schema format to list of dictionary format.
+    """
+    return [{"role": find_role(message),
+             "content": message.content
+             } for message in messages]
+
+
+#  Converte uma lista de dicionário em um formato compatível com o modelo Llama2. 
+def llama_v2_prompt(messages: List[dict]) -> str:
+    """
+    Convert the messages in list of dictionary format to Llama2 compliant format.
+    """
+    B_INST, E_INST = "[INST]", "[/INST]"
+    B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+    BOS, EOS = "<s>", "</s>"
+    DEFAULT_SYSTEM_PROMPT = f"""You are a helful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+   
+   if messages[0]["role"] != "system":
+        messages = [
+            {
+                "role": "system",
+                "content": DEFAULT_SYSTEM_PROMPT,
+            }
+        ] + messages
+    messages = [
+        {
+            "role": messages[1]["role"],
+            "content": B_SYS + messages[0]["content"] + E_SYS + messages[1]["content"],
+        }
+    ] + messages[2:]
+
+    messages_list = [
+        f"{BOS}{B_INST} {(prompt['content']).strip()} {E_INST} {(answer['content']).strip()} {EOS}"
+        for prompt, answer in zip(messages[::2], messages[1::2])
+    ]
+
+    messages_list.append(
+        f"{BOS}{B_INST} {(messages[-1]['content']).strip()} {E_INST}")
+    
+    return "".join(messages_list)
+
+
